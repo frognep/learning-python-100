100b4b71b2b567bd828b02d352b6ad72b322bf87
danny <frognep@gmail.com>
2022-12-18 14:29:36 -0600

    first commit
    pushing past projects for learning progression track

---
+++ hangman/hangman.py
@@ -0,0 +1,53 @@
+import random
+from os import system
+from turtle import clear
+from hangman_words import word_list
+from hangman_art import logo, stages
+
+end_of_game = False
+chosen_word = random.choice(word_list)
+user_lives = 6
+
+print(logo)
+
+# test
+# print(f"pssst, word is {chosen_word}")
+
+# Creates blanks
+display = []
+for char in chosen_word:
+    display += "_"
+
+# Stores user guesses
+guesses = []
+
+while not end_of_game and not user_lives == 0:
+    guess = input("Guess a letter: ").lower()
+
+    system("clear")
+
+    if guess in guesses:
+        print(f"You have already guessed {guess}.")
+    
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if guess == letter:
+            display[position] = guess
+
+    if not guess in chosen_word and guess not in guesses:
+      print(f"{guess} is not in the word. You lose a life.")
+      user_lives -= 1
+    
+    guesses += guess
+
+    # Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    if not "_" in display:
+        end_of_game = True
+        print("You win.")
+    
+    if user_lives == 0:
+        print("You have ran out of lives. You lose.")
+        
+    print(stages[user_lives])
