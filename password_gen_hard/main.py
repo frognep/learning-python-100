44ed325a67cf147ad33dc035d9457a2e460bf85e
danny <frognep@gmail.com>
2022-12-18 14:46:56 -0600

    v0.2
    - import change

--- password_gen_hard/main.py
+++ password_gen_hard/main.py
@@ -1,8 +1,6 @@
 #Password Generator Project
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+import objects
 
 # print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
@@ -15,13 +13,13 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 password = []
 
 for amount_letters in range(nr_letters):
-    password += (random.choice(letters))
+    password += (random.choice(objects.letters))
 
 for amount_symbols in range(nr_symbols):
-    password += (random.choice(symbols))
+    password += (random.choice(objects.symbols))
 
 for amount_numbers in range(nr_numbers):
-    password += random.choice(numbers)
+    password += random.choice(objects.numbers)
     
 # ---------------------
 
